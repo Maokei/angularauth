a597f24eb4ecf1580f73dd24bbae69c23d953d7e
maokei <rickard.joh@gmail.com>
2019-02-01 17:43:23 +0100

    Java spring server initial

---
+++ server_spring/jwtserver_spring/src/main/java/se/maokei/jwtserver_spring/JwtserverSpringApplication.java
@@ -0,0 +1,14 @@
+package se.maokei.jwtserver_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtserverSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtserverSpringApplication.class, args);
+	}
+
+}
+
---
+++ server_spring/jwtserver_spring/src/test/java/se/maokei/jwtserver_spring/JwtserverSpringApplicationTests.java
@@ -0,0 +1,17 @@
+package se.maokei.jwtserver_spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtserverSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
