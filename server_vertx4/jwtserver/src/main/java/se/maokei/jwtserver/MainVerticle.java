089fcd5dd7509f50156fb8982aff743d8478a7b7
maokei <rickard.joh@gmail.com>
2021-11-24 03:34:54 +0100

    graalvm vertx 4

---
+++ server_vertx4/jwtserver/src/main/java/se/maokei/jwtserver/MainVerticle.java
@@ -0,0 +1,28 @@
+package se.maokei.jwtserver;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+
+public class MainVerticle extends AbstractVerticle {
+
+public static void main(String[] args) {
+    Vertx.vertx().deployVerticle(new MainVerticle());
+  }
+
+  @Override
+  public void start(Promise<Void> startPromise) throws Exception {
+    vertx.createHttpServer().requestHandler(req -> {
+      req.response()
+        .putHeader("content-type", "text/plain")
+        .end("Hello from Vert.x!");
+    }).listen(8888, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        System.out.println("HTTP server started on port 8888");
+      } else {
+        startPromise.fail(http.cause());
+      }
+    });
+  }
+}
