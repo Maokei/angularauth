9a37c30d9bf0ee48a3b6e8d70ffd9eabe5e77ea6
maokei <rickard.joh@gmail.com>
2021-11-24 13:08:18 +0100

    With vertx launcher

--- server_vertx4/jwtserver/src/main/java/se/maokei/jwtserver/MainVerticle.java
+++ server_vertx4/jwtserver/src/main/java/se/maokei/jwtserver/MainVerticle.java
@@ -2,13 +2,13 @@ package se.maokei.jwtserver;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
+//import io.vertx.core.Vertx;
 
 public class MainVerticle extends AbstractVerticle {
 
-public static void main(String[] args) {
+/*public static void main(String[] args) {
     Vertx.vertx().deployVerticle(new MainVerticle());
-  }
+}*/
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
