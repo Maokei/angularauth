2a7447eafb2c978c3f8cca6d02cab27add36b7be
maokei <rickard.joh@gmail.com>
2022-01-10 23:05:57 +0100

    Added lombok

--- server_vertx/jwtserver/src/main/java/se/maokei/jwtserver/entity/Event.java
+++ server_vertx/jwtserver/src/main/java/se/maokei/jwtserver/entity/Event.java
@@ -1,4 +1,9 @@
 package se.maokei.jwtserver.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Event {
 }
--- server_vertx/jwtserver/src/main/java/se/maokei/jwtserver/entity/User.java
+++ server_vertx/jwtserver/src/main/java/se/maokei/jwtserver/entity/User.java
@@ -1,28 +1,13 @@
 package se.maokei.jwtserver.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class User {
   private String password;
   private String email;
-
-  public User() {
-
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
 
   @Override
   public String toString() {
