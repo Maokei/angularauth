10b4e8a6872b2f3dfc9c567cf957fe0b12abef44
maokei <rickard.joh@gmail.com>
2022-01-10 23:14:13 +0100

    Java event model

--- server_vertx/jwtserver/src/main/java/se/maokei/jwtserver/entity/Event.java
+++ server_vertx/jwtserver/src/main/java/se/maokei/jwtserver/entity/Event.java
@@ -3,7 +3,13 @@ package se.maokei.jwtserver.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 public class Event {
+  private String _id;
+  private String name;
+  private String description;
+  private Date date;
 }
