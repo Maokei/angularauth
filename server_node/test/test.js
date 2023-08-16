3717f1a4cceb64f7d9562997ac35d15bfe05b762
maokei <rickard.joh@gmail.com>
2023-08-16 07:57:23 +0200

    p

---
+++ server_node/test/test.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+
+describe("bonk", () => {
+    context("Function to be tested", () => {
+        it('should do something', () => {
+            assert.equal(1,1);
+        });
+    });
+});
